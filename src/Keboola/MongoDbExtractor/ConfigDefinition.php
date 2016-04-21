a85c4843397261d4f69ac3f7d4886ac64ca7c38b
Vladimír Kriška <vlado@keboola.com>
2016-04-21 13:29:57 +0200

    Config definition class

---
+++ src/Keboola/MongoDbExtractor/ConfigDefinition.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Keboola\MongoDbExtractor;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Keboola\DbExtractor\Configuration\ConfigDefinition as DbExtractorConfigDefinition;
+
+class ConfigDefinition extends DbExtractorConfigDefinition
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('config');
+
+        $rootNode
+            ->children()
+                ->arrayNode('parameters')
+                    ->children()
+                        ->arrayNode('db')
+                            ->children()
+                                ->scalarNode('host')->end()
+                                ->scalarNode('port')->end()
+                                ->append($this->addSshNode())
+                            ->end()
+                        ->end()
+                        ->arrayNode('exports')
+                            ->prototype('array')
+                                ->children()
+                                    ->scalarNode('name')
+                                        ->isRequired()
+                                        ->cannotBeEmpty()
+                                    ->end()
+                                    ->scalarNode('db')
+                                        ->isRequired()
+                                        ->cannotBeEmpty()
+                                    ->end()
+                                    ->scalarNode('collection')
+                                        ->isRequired()
+                                        ->cannotBeEmpty()
+                                    ->end()
+                                    ->scalarNode('query')
+
+                                    ->end()
+                                    ->arrayNode('fields')
+                                        ->prototype('scalar')->end()
+                                        ->isRequired()
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
