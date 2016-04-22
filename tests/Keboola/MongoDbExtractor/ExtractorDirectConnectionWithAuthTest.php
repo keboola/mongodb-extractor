93bb9fa05d771ac08ea514025e131d91d8fc86aa
Vladimír Kriška <vlado@keboola.com>
2016-04-22 20:25:48 +0200

    Tests: Testing connection to db with auth support

---
+++ tests/Keboola/MongoDbExtractor/ExtractorDirectConnectionWithAuthTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Keboola\MongoDbExtractor;
+
+use Keboola\Test\ExtractorTestCase;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Yaml\Yaml;
+
+class ExtractorDirectConnectionWithAuthTest extends ExtractorTestCase
+{
+    /** @var Filesystem */
+    private $fs;
+
+    protected $path = '/tmp/extractor-direct-auth';
+
+    protected function setUp()
+    {
+        $this->fs = new Filesystem;
+        $this->fs->remove($this->path);
+        $this->fs->mkdir($this->path);
+
+        parent::setUp();
+    }
+
+    protected function tearDown()
+    {
+        $this->fs->remove($this->path);
+    }
+
+    protected function getConfig()
+    {
+        $config = <<<YAML
+parameters:
+  db:
+    host: mongodb-auth
+    port: 27017
+    user: user
+    password: user
+YAML;
+        return Yaml::parse($config);
+
+    }
+}
