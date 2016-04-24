0e917dbeb0e30e93e165de7f1cc0064a5410ace0
Vladimír Kriška <vlado@keboola.com>
2016-04-24 22:00:38 +0200

    Deploy on tags
    Fixes #7

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag keboola/mongodb-extractor quay.io/keboola/mongodb-extractor:latest
+docker tag keboola/mongodb-extractor quay.io/keboola/mongodb-extractor:$TRAVIS_TAG
 docker images
-docker push quay.io/keboola/mongodb-extractor:latest
+docker push quay.io/keboola/mongodb-extractor:$TRAVIS_TAG
