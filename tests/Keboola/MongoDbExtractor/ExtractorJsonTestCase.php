8e85c332760b329cb1e93a196329076fce5fa54e
Vladimír Kriška <vlado@keboola.com>
2016-05-03 14:47:05 +0200

    Tests for fields+json

--- tests/Keboola/MongoDbExtractor/ExtractorJsonTestCase.php
+++ tests/Keboola/MongoDbExtractor/ExtractorJsonTestCase.php
@@ -23,6 +23,11 @@ abstract class ExtractorJsonTestCase extends \PHPUnit_Framework_TestCase
     {
         $exportParams = [
             'collection' => 'restaurants',
+            'fields' => [
+                'borough',
+                'cuisine',
+                'name',
+            ],
             'name' => 'export-all',
         ];
 
@@ -46,6 +51,11 @@ abstract class ExtractorJsonTestCase extends \PHPUnit_Framework_TestCase
     {
         $exportParams = [
             'collection' => 'restaurants',
+            'fields' => [
+                'borough',
+                'cuisine',
+                'name',
+            ],
             'query' => '{_id: ObjectId("5716054bee6e764c94fa7ddd")}',
             'name' => 'export-one',
         ];
@@ -58,7 +68,7 @@ abstract class ExtractorJsonTestCase extends \PHPUnit_Framework_TestCase
         $this->assertTrue($export, 'Command successful');
 
         $expectedJson = <<<JSON
-[{"_id":{"\$oid":"5716054bee6e764c94fa7ddd"},"address":{"building":"1007","coord":[-73.856077,40.848447],"street":"Morris Park Ave","zipcode":"10462"},"borough":"Bronx","cuisine":"Bakery","name":"Morris Park Bake Shop"}]\n
+[{"_id":{"\$oid":"5716054bee6e764c94fa7ddd"},"borough":"Bronx","cuisine":"Bakery","name":"Morris Park Bake Shop"}]\n
 JSON;
 
         $expectedFile = $this->path . '/' . 'export-one.json';
@@ -72,6 +82,11 @@ JSON;
     {
         $exportParams = [
             'collection' => 'restaurants',
+            'fields' => [
+                'borough',
+                'cuisine',
+                'name',
+            ],
             'query' => '{borough : "Bronx", cuisine: "Bakery", "address.zipcode": "10452"}',
             'name' => 'export-multi',
         ];
@@ -84,7 +99,7 @@ JSON;
         $this->assertTrue($export, 'Command successful');
 
         $expectedJson = <<<JSON
-[{"_id":{"\$oid":"5716054bee6e764c94fa8c8b"},"address":{"building":"1193","coord":[-73.9197389,40.83489170000001],"street":"Walton Avenue","zipcode":"10452"},"borough":"Bronx","cuisine":"Bakery","name":"Nb. National Bakery"},{"_id":{"\$oid":"5716054cee6e764c94faba0d"},"address":{"building":"155","coord":[-73.9147942,40.83937700000001],"street":"East 170 Street","zipcode":"10452"},"borough":"Bronx","cuisine":"Bakery","name":"La Rosa Bakery"},{"_id":{"\$oid":"5716054cee6e764c94fad056"},"address":{"building":"6A","coord":[-73.9188034,40.8381439],"street":"East Clarke Place","zipcode":"10452"},"borough":"Bronx","cuisine":"Bakery","name":"Emilio Super Bakery Corp"}]\n
+[{"_id":{"\$oid":"5716054bee6e764c94fa8c8b"},"borough":"Bronx","cuisine":"Bakery","name":"Nb. National Bakery"},{"_id":{"\$oid":"5716054cee6e764c94faba0d"},"borough":"Bronx","cuisine":"Bakery","name":"La Rosa Bakery"},{"_id":{"\$oid":"5716054cee6e764c94fad056"},"borough":"Bronx","cuisine":"Bakery","name":"Emilio Super Bakery Corp"}]\n
 JSON;
 
         $expectedFile = $this->path . '/' . 'export-multi.json';
@@ -97,6 +112,14 @@ JSON;
     {
         $exportParams = [
             'collection' => 'restaurants',
+            'fields' => [
+                'borough',
+                'cuisine',
+                'name',
+                'address.street',
+                'address.zipcode',
+                'address.building',
+            ],
             'query' => '{borough : "Bronx", cuisine: "Bakery", "address.zipcode": "10452"}',
             'name' => 'export-multi-fields-paths',
         ];
@@ -123,6 +146,12 @@ JSON;
     {
         $exportParams = [
             'collection' => 'restaurants',
+            'fields' => [
+                'name',
+                'borough',
+                'address.street',
+                'address.zipcode',
+            ],
             'query' => '{name: "National Bakery"}',
             'sort' => '{"address.street": 1}',
             'limit' => 3,
@@ -137,7 +166,7 @@ JSON;
         $this->assertTrue($export, 'Command successful');
 
         $expectedJson = <<<JSON
-[{"_id":{"\$oid":"5716054bee6e764c94fa9620"},"address":{"building":"767","coord":[-73.86468099999999,40.865699],"street":"Allerton Avenue","zipcode":"10467"},"borough":"Bronx","cuisine":"Bakery","name":"National Bakery"},{"_id":{"\$oid":"5716054bee6e764c94fa93f9"},"address":{"building":"944","coord":[-73.8965138,40.8212482],"street":"Intelvale Avenue","zipcode":"10459"},"borough":"Bronx","cuisine":"Bakery","name":"National Bakery"},{"_id":{"\$oid":"5716054bee6e764c94fa8ff6"},"address":{"building":"1522-4","coord":[-73.8789604,40.8286012],"street":"Westchester Avenue","zipcode":"10472"},"borough":"Bronx","cuisine":"Bakery","name":"National Bakery"}]\n
+[{"_id":{"\$oid":"5716054bee6e764c94fa9620"},"address":{"building":"767","coord":[-73.86468099999999,40.865699],"street":"Allerton Avenue","zipcode":"10467"},"borough":"Bronx","name":"National Bakery"},{"_id":{"\$oid":"5716054bee6e764c94fa93f9"},"address":{"building":"944","coord":[-73.8965138,40.8212482],"street":"Intelvale Avenue","zipcode":"10459"},"borough":"Bronx","name":"National Bakery"},{"_id":{"\$oid":"5716054bee6e764c94fa8ff6"},"address":{"building":"1522-4","coord":[-73.8789604,40.8286012],"street":"Westchester Avenue","zipcode":"10472"},"borough":"Bronx","name":"National Bakery"}]\n
 JSON;
 
         $expectedFile = $this->path . '/' . 'export-multi-with-sort-and-limit.json';
@@ -152,6 +181,11 @@ JSON;
 
         $exportParams = [
             'collection' => 'restaurants',
+            'fields' => [
+                'borough',
+                'cuisine',
+                'name',
+            ],
             'query' => '{a: b}', // invalid JSON
             'name' => 'export-bad-query',
         ];
@@ -166,6 +200,11 @@ JSON;
     {
         $exportParams = [
             'collection' => 'randomCollection',
+            'fields' => [
+                'borough',
+                'cuisine',
+                'name',
+            ],
             'query' => '{_id: ObjectId("5716054bee6e764c94fa7ddd")}',
             'name' => 'export-random-database',
         ];
