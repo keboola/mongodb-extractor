025afd41dd637a6e0125ad283422ad843efe386c
Vladimír Kriška <vlado@keboola.com>
2017-02-18 16:48:16 +0100

    Handle user/app exceptions better. Hide error output.

--- src/run.php
+++ src/run.php
@@ -4,22 +4,25 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/common.php';
 
 use Keboola\MongoDbExtractor\Application;
+use Monolog\Logger;
+use Monolog\Handler\ErrorLogHandler;
+use Keboola\MongoDbExtractor\UserException;
 use Symfony\Component\Serializer\Encoder\JsonDecode;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 
-$arguments = getopt('', ['data:']);
-if (!isset($arguments['data'])) {
-    echo 'Data folder not set.' . "\n";
-    exit(2);
-}
-
-$configFile = $arguments['data'] . '/config.json';
-if (!file_exists($configFile)) {
-    echo 'Config file not found' . "\n";
-    exit(2);
-}
+$logger = new Logger('app-errors', [new ErrorLogHandler]);
 
 try {
+    $arguments = getopt('', ['data:']);
+    if (!isset($arguments['data'])) {
+        throw new Exception('Data folder not set.');
+    }
+
+    $configFile = $arguments['data'] . '/config.json';
+    if (!file_exists($configFile)) {
+        throw new Exception('Config file not found.');
+    }
+
     $jsonDecode = new JsonDecode(true);
     $config = $jsonDecode->decode(
         file_get_contents($arguments['data'] . '/config.json'),
@@ -41,7 +44,15 @@ try {
     }
 
     exit(0);
-} catch (Exception $e) {
+} catch (UserException $e) {
     echo $e->getMessage();
     exit(1);
+} catch (MongoDB\Driver\Exception\ConnectionTimeoutException $e) {
+    echo $e->getMessage();
+    exit(1);
+} catch (\Exception $e) {
+    $logger->error($e->getMessage(), [
+        'trace' => $e->getTraceAsString()
+    ]);
+    exit(2);
 }
