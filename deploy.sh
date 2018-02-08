8a84197f7a795eddb6e7d2815e9d6341ac15547f
Vladimír Kriška <vlado@keboola.com>
2018-02-08 18:40:49 +0100

    Use set -e instead of && in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-DP_VENDOR_ID="keboola"
-DP_APP_ID="keboola.ex-mongodb"
+set -e
 
-docker pull quay.io/keboola/developer-portal-cli-v2:latest \
-&& export REPOSITORY=`docker run --rm \
+DEVPORTAL_VENDOR_ID="keboola"
+DEVPORTAL_APP_ID="keboola.ex-mongodb"
+
+docker pull quay.io/keboola/developer-portal-cli-v2:latest
+
+export REPOSITORY=`docker run --rm \
   -e KBC_DEVELOPERPORTAL_USERNAME=$DP_USERNAME \
   -e KBC_DEVELOPERPORTAL_PASSWORD=$DP_PASSWORD \
-  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository $DP_VENDOR_ID $DP_APP_ID` \
-&& docker tag keboola/mongodb-extractor $REPOSITORY:$TRAVIS_TAG \
-&& docker tag keboola/mongodb-extractor $REPOSITORY:latest \
-&& docker images \
-&& eval $(docker run --rm \
+  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository $DEVPORTAL_VENDOR_ID $DEVPORTAL_APP_ID`
+
+docker tag keboola/mongodb-extractor $REPOSITORY:$TRAVIS_TAG
+docker tag keboola/mongodb-extractor $REPOSITORY:latest
+docker images
+
+eval $(docker run --rm \
   -e KBC_DEVELOPERPORTAL_USERNAME=$DP_USERNAME \
   -e KBC_DEVELOPERPORTAL_PASSWORD=$DP_PASSWORD \
-  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login $DP_VENDOR_ID $DP_APP_ID) \
-&& docker push $REPOSITORY:$TRAVIS_TAG \
-&& docker push $REPOSITORY:latest \
-&& docker run --rm \
+  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login $DEVPORTAL_VENDOR_ID $DEVPORTAL_APP_ID)
+
+docker push $REPOSITORY:$TRAVIS_TAG
+docker push $REPOSITORY:latest
+
+docker run --rm \
   -e KBC_DEVELOPERPORTAL_USERNAME=$DP_USERNAME \
   -e KBC_DEVELOPERPORTAL_PASSWORD=$DP_PASSWORD \
-  quay.io/keboola/developer-portal-cli-v2:latest update-app-repository $DP_VENDOR_ID $DP_APP_ID $TRAVIS_TAG
+  quay.io/keboola/developer-portal-cli-v2:latest update-app-repository $DEVPORTAL_VENDOR_ID $DEVPORTAL_APP_ID $TRAVIS_TAG
