4905c1fe7a26ec239f91f43ef9f5c24d5a82e6b0
Vladimír Kriška <vlado@keboola.com>
2018-02-08 18:45:09 +0100

    Use env variables with same name

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ DEVPORTAL_APP_ID="keboola.ex-mongodb"
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 
 export REPOSITORY=`docker run --rm \
-  -e KBC_DEVELOPERPORTAL_USERNAME=$DP_USERNAME \
-  -e KBC_DEVELOPERPORTAL_PASSWORD=$DP_PASSWORD \
+  -e KBC_DEVELOPERPORTAL_USERNAME \
+  -e KBC_DEVELOPERPORTAL_PASSWORD \
   quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository $DEVPORTAL_VENDOR_ID $DEVPORTAL_APP_ID`
 
 docker tag keboola/mongodb-extractor $REPOSITORY:$TRAVIS_TAG
@@ -17,14 +17,14 @@ docker tag keboola/mongodb-extractor $REPOSITORY:latest
 docker images
 
 eval $(docker run --rm \
-  -e KBC_DEVELOPERPORTAL_USERNAME=$DP_USERNAME \
-  -e KBC_DEVELOPERPORTAL_PASSWORD=$DP_PASSWORD \
+  -e KBC_DEVELOPERPORTAL_USERNAME \
+  -e KBC_DEVELOPERPORTAL_PASSWORD \
   quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login $DEVPORTAL_VENDOR_ID $DEVPORTAL_APP_ID)
 
 docker push $REPOSITORY:$TRAVIS_TAG
 docker push $REPOSITORY:latest
 
 docker run --rm \
-  -e KBC_DEVELOPERPORTAL_USERNAME=$DP_USERNAME \
-  -e KBC_DEVELOPERPORTAL_PASSWORD=$DP_PASSWORD \
+  -e KBC_DEVELOPERPORTAL_USERNAME \
+  -e KBC_DEVELOPERPORTAL_PASSWORD \
   quay.io/keboola/developer-portal-cli-v2:latest update-app-repository $DEVPORTAL_VENDOR_ID $DEVPORTAL_APP_ID $TRAVIS_TAG
