f0e117a0f07ee370ae5188b838b2bfe8ecbcb318
Vladimír Kriška <vlado@keboola.com>
2018-02-08 18:28:36 +0100

    Remove KBC_DEVELOPERPORTAL_URL from deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,6 @@ docker pull quay.io/keboola/developer-portal-cli-v2:latest \
 && export REPOSITORY=`docker run --rm \
   -e KBC_DEVELOPERPORTAL_USERNAME=$DP_USERNAME \
   -e KBC_DEVELOPERPORTAL_PASSWORD=$DP_PASSWORD \
-  -e KBC_DEVELOPERPORTAL_URL=$DP_URL \
   quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository $DP_VENDOR_ID $DP_APP_ID` \
 && docker tag keboola/mongodb-extractor $REPOSITORY:$TRAVIS_TAG \
 && docker tag keboola/mongodb-extractor $REPOSITORY:latest \
@@ -15,12 +14,10 @@ docker pull quay.io/keboola/developer-portal-cli-v2:latest \
 && eval $(docker run --rm \
   -e KBC_DEVELOPERPORTAL_USERNAME=$DP_USERNAME \
   -e KBC_DEVELOPERPORTAL_PASSWORD=$DP_PASSWORD \
-  -e KBC_DEVELOPERPORTAL_URL=$DP_URL \
   quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login $DP_VENDOR_ID $DP_APP_ID) \
 && docker push $REPOSITORY:$TRAVIS_TAG \
 && docker push $REPOSITORY:latest \
 && docker run --rm \
   -e KBC_DEVELOPERPORTAL_USERNAME=$DP_USERNAME \
   -e KBC_DEVELOPERPORTAL_PASSWORD=$DP_PASSWORD \
-  -e KBC_DEVELOPERPORTAL_URL=$DP_URL \
   quay.io/keboola/developer-portal-cli-v2:latest update-app-repository $DP_VENDOR_ID $DP_APP_ID $TRAVIS_TAG
